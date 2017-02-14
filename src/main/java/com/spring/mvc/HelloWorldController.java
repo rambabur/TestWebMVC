f650ed5b7abd46227be6b75517db6de36586ad14
RAMABABU RAVURI <RAMABABU RAVURI>
2017-02-13 19:10:49 -0600

    My first commit

---
+++ src/main/java/com/spring/mvc/HelloWorldController.java
@@ -0,0 +1,24 @@
+package com.spring.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class HelloWorldController {
+
+	@RequestMapping(method = RequestMethod.GET)
+	public String sayHello(ModelMap model) {
+		model.addAttribute("greeting", "Hello Testing my git first");
+		return "welcome";
+	}
+
+	@RequestMapping(value = "/helloagain", method = RequestMethod.GET)
+	public String sayHelloAgain(ModelMap model) {
+		model.addAttribute("greeting", "Hello Git, from Spring 4 MVC from rajitha");
+		return "welcome";
+	}
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,12 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+Date :
+<% 
+
+java.util.Date date = new java.util.Date();
+out.println("<h4> Date " + date.toString());
+
+ %>
+</body>
+</html>
